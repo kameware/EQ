46ccb28e764df8604acea93368e7711b0b7443b4
takanari nakamura <nakamura@minemine.local>
2015-09-02 22:17:40 +0900

    objective-cでイコライザー

---
+++ EQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EQ
+//
+//  Created by mineharu on 2015/09/01.
+//  Copyright (c) 2015年 mineharu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
